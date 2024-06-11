ab0a995e47a92777348f917b5c51cac580ad3f2e
K4mp47 <campagnoloalberto5@gmail.com>
2024-06-11 12:00:13 +0200

    chore: responsive mobile updates

--- src/App.jsx
+++ src/App.jsx
@@ -15,32 +15,33 @@ function App() {
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }} className='mx-auto -z-5 sm:scale-100'>
         <ThreeComponent />
       </div>
-      <a href="https://github.com/K4mp47">
-        <div className='font-mono absolute m-4 md:m-10 md:mb-[8.4rem] mb-[4.6rem] z-1 text-[0.6rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
+      <div className='font-mono absolute m-4 md:m-10 md:mb-[8.4rem] mb-[6.2rem] z-1 text-[1rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
         Hi, I'm a
-        </div>
-      </a>
-      <div className='absolute m-4 md:m-10 md:mb-28 mb-14 z-1 text-md md:text-2xl left-0 bottom-0'>
+      </div>
+      <div className='absolute m-4 md:m-10 md:mb-28 mb-[5rem] z-1 text-xl md:text-2xl left-0 bottom-0'>
         Web Developer 
       </div>
-      <div className='font-mono absolute m-4 md:m-10 md:mb-20 mb-10 z-1 text-[0.5rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
+      <div className='font-mono absolute m-4 md:m-10 md:mb-20 mb-[3.3rem] z-1 text-[1rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
         Local Time (UTC+1)
       </div>
-      <div className='absolute m-4 md:m-10 md:mb-14 mb-[1.45rem] z-1 text-sm md:text-2xl left-0 bottom-0'>
+      <div className='absolute m-4 md:m-10 md:mb-14 mb-[2rem] z-1 text-xl md:text-2xl left-0 bottom-0'>
         <Clock />
       </div>
-      <div className='absolute m-4 md:m-10 md:mb-14 mb-[1.4rem] z-1 ml-32 md:ml-56 font-mono text-[0.5rem] p-[0.09rem] md:p-[0.16rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
+      <div className='absolute m-4 md:m-10 md:mb-14 mb-[2rem] z-1 ml-40 md:ml-56 font-mono text-[1rem] p-[0.09rem] md:p-[0.16rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
         <FPS />
       </div>
-      <div className='font-mono absolute m-4 md:m-10 z-1 text-[0.5rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
+      <div className='font-mono absolute m-4 md:m-10 z-1 text-[1rem] md:text-[1rem] text-gray-400 left-0 bottom-0'>
         Treviso, Veneto &#x21AA; Italy
       </div>
       <div className='absolute flex flex-col m-4 md:m-10 z-1 text-md right-0 bottom-0'>
+        <a href="https://github.com/K4mp47">
+          <FaGithub size={isMediumScreen ? 25 : 27} />
+        </a>
         <a href="https://www.instagram.com/isthatcampa/?next=%2F">
-          <FaInstagram size={isMediumScreen ? 25 : 15} />
+          <FaInstagram size={isMediumScreen ? 25 : 27} />
         </a>
         <a href="https://www.linkedin.com/in/alberto-campagnolo-916b86265/">
-          <FaLinkedin size={isMediumScreen ? 25 : 15} />
+          <FaLinkedin size={isMediumScreen ? 25 : 27} />
         </a>
       </div>
     </div>
